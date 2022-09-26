8efe06bb3a5c398dfa294c7cb2ba47259fda5a67
praveenlingam2 <praveen121lingam@gmail.com>
2022-09-26 17:06:10 +0530

    java

--- src/main/java/BinaryValue.java
+++ src/main/java/BinaryValue.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class BinaryValue {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
-        int b=scanner.nextInt();
-        int flag=0;
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt(), b = scanner.nextInt(), flag = 0;
         String result = Integer.toBinaryString(a);
         String a1 = String.format("%8s", result).replaceAll(" ", "0");
         System.out.println(a1);
         String results = Integer.toBinaryString(b);
         String b1 = String.format("%8s", results).replaceAll(" ", "0");
         System.out.println(b1);
-        int minLen = Math.min(a1.length(),b1.length());
-        for (int i = 0 ; i != minLen ; i++) {
-            char chaA=a1.charAt(i);
-            char chaB=b1.charAt(i);
-            if(chaA!=chaB){
+        char[] arrA = a1.toCharArray();
+        char[] arrB = b1.toCharArray();
+        for (int i = 0; i < arrA.length; i++) {
+            if (arrA[i] != arrB[i]) {
                 flag++;
             }
         }
