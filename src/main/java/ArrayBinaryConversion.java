89d6b954415220a4615bbed284ec351ff0cd76d2
praveenlingam2 <praveen121lingam@gmail.com>
2022-09-30 14:47:19 +0530

    java

---
+++ src/main/java/ArrayBinaryConversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayBinaryConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        char[] arr=str.toCharArray();
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        float avg=sum/arr.length;
+        System.out.println(avg);
+    }
+}
